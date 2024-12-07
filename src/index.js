59b91a1a6939255e51bb3e799ce670916753108c
unknown <zignale92@gmail.com>
2024-12-07 21:10:23 +0100

    Rating API

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom/client';
 //import App from './App';
 import StartRating from './StartRating';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    {/*<App />*/} 
-   <StartRating maxRating={5} />
+   <StartRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+   <StartRating size={24} color='red' className="test" defaultRating={3} />
   </React.StrictMode>
 );
 
