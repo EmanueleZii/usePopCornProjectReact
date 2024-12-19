081042077efbb49e772ed106cbb09eb56769a63d
unknown <zignale92@gmail.com>
2024-12-19 12:06:20 +0100

    v1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
-//import App from './App';
+import './index.css';
+import App from './App';
 import StartRating from './StartRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   {/*<App />*/} 
-   <StartRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
-   <StartRating size={24} color='red' className="test" defaultRating={3} />
+   <App /> 
+   {/*<StartRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+   <StartRating size={24} color='red' className="test" defaultRating={3} />*/}
   </React.StrictMode>
 );
 
