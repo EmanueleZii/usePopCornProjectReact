e0972a1f124d2ce01fc5ef3fc79d1a0b68033ddc
unknown <zignale92@gmail.com>
2025-02-12 12:07:18 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './App.css';
 import App from './App';
 import StartRating from './StartRating';
-import AppV1 from './AppV1';
+//import AppV1 from './AppV1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <AppV1 /> 
+    <App/> 
    </React.StrictMode>
 );
 
