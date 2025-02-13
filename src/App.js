a0aa385d05b11fd36ab4cbb9c5ab5a79ba23c54c
unknown <zignale92@gmail.com>
2025-02-13 10:56:51 +0100

    apikey

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { useMovies } from "./useMovies";
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "f84fc31d";
+const KEY = "ef22e03e";
 
 export default function App() {
   const [query, setQuery] = useState("");
@@ -173,6 +173,7 @@ function WatchedBox() {
   );
 }
 */
+
 
 function MovieList({ movies, onSelectMovie }) {
   return (
--- src/useMovies.js
+++ src/useMovies.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const KEY = "f84fc31d";
+const KEY = "ef22e03e";
 
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
